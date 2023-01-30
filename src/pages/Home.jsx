94c4eafe713eb2d71b5ed469bab90b4c63705bf3
Augusto Sergio <augusto_spr@hotmail.com>
2023-01-30 18:58:45 -0300

    Did the fetch for the name and make the structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,47 @@
-import React from "react";
-
+import React, { useEffect } from "react";
 import CardPokemon from "../components/cardPokemon/CardPokemon";
 import Cabecalho from "../components/cabecalho/Cabecalho";
 import PesquisaPokemon from "../components/pesquisaPokemon/PesquisaPokemon";
+import { Container, Grid } from "@mui/material";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [api, setApi] = useState([]);
+
+  useEffect(() => {
+    getApi();
+  }, []);
+
+  const getApi = () => {
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=50&offset=0/`)
+      .then((res) => res.json())
+      .then((res) => setApi(res.results))
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  console.log(api);
+
+
   return (
     <>
-      <Cabecalho />
+      <Container>
+        <Grid container spacing={2}>
+          <Cabecalho />
+          <PesquisaPokemon />
 
-      <PesquisaPokemon />
 
-      <CardPokemon />
+          {api.map((item, key) => (
+            <Grid item key={key}>
+              <CardPokemon nome={item.name} />
+            </Grid>
+          ))}
+
+
+        </Grid>
+      </Container>
 
     </>
   )
