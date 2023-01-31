6d70d5289723f496fca34af9d11efa3bcc7333c0
Augusto Sergio <augusto_spr@hotmail.com>
2023-01-31 18:52:44 -0300

    try to fetch all links using promise.all

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,20 +10,34 @@ export default function Home() {
   const [api, setApi] = useState([]);
 
   useEffect(() => {
-    getApi();
+    // getApi();
+    getApiData();
   }, []);
 
-  const getApi = () => {
-    fetch(`https://pokeapi.co/api/v2/pokemon?limit=50&offset=0/`)
-      .then((res) => res.json())
-      .then((res) => setApi(res.results))
-      .catch((err) => {
-        console.log(err);
-      });
+  // const getApi = async () => {
+  //   try {
+  //     await fetch(`https://pokeapi.co/api/v2/pokemon?limit=50&offset=0/`)
+  //       .then((res) => res.json())
+  //       .then((res) => setApi(res.results))
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // }
+
+  const getApiData = async () => {
+    try {
+      const endpoints = [];
+      for (var i = 1; i < 50; i++) {
+        endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+      }
+      const resposta = Promise.all(endpoints.map((endpoint) => fetch(endpoint))).then((res) => setApi(res));
+      console.log(resposta);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   console.log(api);
-
 
   return (
     <>
